13f9461732c1e0005e5574007bd78520c875af53
misterelgo <misterelgo@gmail.com>
2022-10-26 18:26:02 +0200

    creating a graph definition

--- main.py
+++ main.py
@@ -2,13 +2,31 @@ import requests
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
+# 1- Create your user account
+USERNAME = "elgo"
+TOKEN = "osghfugofnbf8HJ7gfg"
 user_params = {
-    "token": "osghfugofnbf8HJ7gfg",
-    "username": "elgo",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
 
-response = requests.post(url=pixela_endpoint, json=user_params)
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
 
+# 2- Create a graph definition
+graph_endpoints = f"{pixela_endpoint}/{USERNAME}/graphs"
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+graph_configs = {
+    "id": "graph1",
+    "name": "Reading Graph",
+    "unit": "km",
+    "type": "float",
+    "color": "shibafu"
+}
+
+response = requests.post(url=graph_endpoints, json=graph_configs, headers=headers)
 print(response.text)
